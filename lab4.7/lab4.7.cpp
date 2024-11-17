f11778d7013a28751ae06e1f819215aec6547956
X13X666X <newmicrogamer@gmail.com>
2024-11-18 01:48:31 +0200

    upload

---
+++ lab4.7/lab4.7.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+int main() {
+    double xp, xk, x, dx, eps, a, R, S;
+    int n;
+
+    cout << "xp = "; cin >> xp;
+    cout << "xk = "; cin >> xk;
+    cout << "dx = "; cin >> dx;
+    cout << "eps = "; cin >> eps;
+
+    cout << fixed;
+    cout << "-------------------------------------------------" << endl;
+    cout << "|" << setw(7) << "x" << " |"
+        << setw(10) << "cos(x)" << " |"
+        << setw(10) << "S" << " |"
+        << setw(5) << "n" << " |" << endl;
+    cout << "-------------------------------------------------" << endl;
+
+    x = xp;
+    while (x <= xk) {
+        n = 0;
+        a = 1; 
+        S = a;
+
+        do {
+            n++;
+            R = -(x * x) / ((2 * n) * (2 * n - 1)); 
+            a *= R;
+            S += a;
+        } while (fabs(a) >= eps); 
+
+        cout << "|" << setw(7) << setprecision(2) << x << " |"
+            << setw(10) << setprecision(5) << cos(x) << " |"
+            << setw(10) << setprecision(5) << S << " |"
+            << setw(5) << n << " |" << endl;
+
+        x += dx;
+    }
+    cout << "-------------------------------------------------" << endl;
+    return 0;
+}
